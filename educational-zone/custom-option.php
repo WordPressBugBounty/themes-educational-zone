17a0e875572a6b6e991afd10650f17770e2f8a9b
Sean Murphy <sean@iamseanmurphy.com>
2024-11-22 04:33:16 +0000

    0.5.9

---
+++ educational-zone/custom-option.php
@@ -0,0 +1,153 @@
+<?php
+
+    $educational_zone_theme_css= "";
+
+    /*--------------------------- Scroll To Top Positions -------------------*/
+
+    $educational_zone_scroll_position = get_theme_mod( 'educational_zone_scroll_top_position','Right');
+    if($educational_zone_scroll_position == 'Right'){
+        $educational_zone_theme_css .='#button{';
+            $educational_zone_theme_css .='right: 20px;';
+        $educational_zone_theme_css .='}';
+    }else if($educational_zone_scroll_position == 'Left'){
+        $educational_zone_theme_css .='#button{';
+            $educational_zone_theme_css .='left: 20px;';
+        $educational_zone_theme_css .='}';
+    }else if($educational_zone_scroll_position == 'Center'){
+        $educational_zone_theme_css .='#button{';
+            $educational_zone_theme_css .='right: 50%;left: 50%;';
+        $educational_zone_theme_css .='}';
+    }
+
+    /*--------------------------- Woocommerce Product Sale Positions -------------------*/
+
+    $educational_zone_product_sale = get_theme_mod( 'educational_zone_woocommerce_product_sale','Right');
+    if($educational_zone_product_sale == 'Right'){
+        $educational_zone_theme_css .='.woocommerce ul.products li.product .onsale{';
+            $educational_zone_theme_css .='left: auto; right: 15px;';
+        $educational_zone_theme_css .='}';
+    }else if($educational_zone_product_sale == 'Left'){
+        $educational_zone_theme_css .='.woocommerce ul.products li.product .onsale{';
+            $educational_zone_theme_css .='left: 15px; right: auto;';
+        $educational_zone_theme_css .='}';
+    }else if($educational_zone_product_sale == 'Center'){
+        $educational_zone_theme_css .='.woocommerce ul.products li.product .onsale{';
+            $educational_zone_theme_css .='right: 50%; left: 50%; letter-spacing: 0px; padding: 0px 0px; font-size: 12px;';
+        $educational_zone_theme_css .='}';
+    }
+
+    /*--------------------------- Woocommerce Product Border Radius -------------------*/
+
+    $educational_zone_woo_product_border_radius = get_theme_mod('educational_zone_woo_product_border_radius', 0);
+    if($educational_zone_woo_product_border_radius != false){
+        $educational_zone_theme_css .='.woocommerce ul.products li.product a img{';
+            $educational_zone_theme_css .='border-radius: '.esc_attr($educational_zone_woo_product_border_radius).'px;';
+        $educational_zone_theme_css .='}';
+    }
+
+    /*---------------- Single post Settings ------------------*/
+
+    $educational_zone_single_post_navigation_show_hide = get_theme_mod('educational_zone_single_post_navigation_show_hide',true);
+    if($educational_zone_single_post_navigation_show_hide != true){
+        $educational_zone_theme_css .='.nav-links{';
+            $educational_zone_theme_css .='display: none;';
+        $educational_zone_theme_css .='}';
+    }
+
+    /*--------------------------- Footer Background Image Position -------------------*/
+
+    $educational_zone_footer_bg_image_position = get_theme_mod( 'educational_zone_footer_bg_image_position','scroll');
+    if($educational_zone_footer_bg_image_position == 'fixed'){
+        $educational_zone_theme_css .='#colophon{';
+            $educational_zone_theme_css .='background-attachment: fixed !important; background-position: center !important;';
+        $educational_zone_theme_css .='}';
+    }elseif ($educational_zone_footer_bg_image_position == 'scroll'){
+        $educational_zone_theme_css .='#colophon{';
+            $educational_zone_theme_css .='background-attachment: scroll !important; background-position: center !important;';
+        $educational_zone_theme_css .='}';
+    }
+
+    /*--------------------------- Footer Widget Heading Alignment -------------------*/
+
+    $educational_zone_footer_widget_heading_alignment = get_theme_mod( 'educational_zone_footer_widget_heading_alignment','Left');
+    if($educational_zone_footer_widget_heading_alignment == 'Left'){
+        $educational_zone_theme_css .='#colophon h5, h5.footer-column-widget-title{';
+        $educational_zone_theme_css .='text-align: left;';
+        $educational_zone_theme_css .='}';
+    }else if($educational_zone_footer_widget_heading_alignment == 'Center'){
+        $educational_zone_theme_css .='#colophon h5, h5.footer-column-widget-title{';
+            $educational_zone_theme_css .='text-align: center;';
+        $educational_zone_theme_css .='}';
+    }else if($educational_zone_footer_widget_heading_alignment == 'Right'){
+        $educational_zone_theme_css .='#colophon h5, h5.footer-column-widget-title{';
+            $educational_zone_theme_css .='text-align: right;';
+        $educational_zone_theme_css .='}';
+    }
+
+    /*--------------------------- Footer background image -------------------*/
+
+    $educational_zone_footer_bg_image = get_theme_mod('educational_zone_footer_bg_image');
+    if($educational_zone_footer_bg_image != false){
+        $educational_zone_theme_css .='#colophon{';
+            $educational_zone_theme_css .='background: url('.esc_attr($educational_zone_footer_bg_image).');';
+        $educational_zone_theme_css .='}';
+    }
+
+    /*--------------------------- Copyright Background Color -------------------*/
+
+    $educational_zone_copyright_background_color = get_theme_mod('educational_zone_copyright_background_color');
+    if($educational_zone_copyright_background_color != false){
+        $educational_zone_theme_css .='.footer_info{';
+            $educational_zone_theme_css .='background-color: '.esc_attr($educational_zone_copyright_background_color).' !important;';
+        $educational_zone_theme_css .='}';
+    } 
+
+    /*--------------------------- Site Title And Tagline Color -------------------*/
+
+    $educational_zone_logo_title_color = get_theme_mod('educational_zone_logo_title_color');
+    if($educational_zone_logo_title_color != false){
+        $educational_zone_theme_css .='p.site-title a, .navbar-brand a{';
+            $educational_zone_theme_css .='color: '.esc_attr($educational_zone_logo_title_color).' !important;';
+        $educational_zone_theme_css .='}';
+    }
+
+    $educational_zone_logo_tagline_color = get_theme_mod('educational_zone_logo_tagline_color');
+    if($educational_zone_logo_tagline_color != false){
+        $educational_zone_theme_css .='.logo p.site-description, .navbar-brand p{';
+            $educational_zone_theme_css .='color: '.esc_attr($educational_zone_logo_tagline_color).'  !important;';
+        $educational_zone_theme_css .='}';
+    }
+
+    /*--------------------------- Footer Widget Content Alignment -------------------*/
+
+    $educational_zone_footer_widget_content_alignment = get_theme_mod( 'educational_zone_footer_widget_content_alignment','Left');
+    if($educational_zone_footer_widget_content_alignment == 'Left'){
+        $educational_zone_theme_css .='#colophon ul, #colophon p, .tagcloud, .widget{';
+        $educational_zone_theme_css .='text-align: left;';
+        $educational_zone_theme_css .='}';
+    }else if($educational_zone_footer_widget_content_alignment == 'Center'){
+        $educational_zone_theme_css .='#colophon ul, #colophon p, .tagcloud, .widget{';
+            $educational_zone_theme_css .='text-align: center;';
+        $educational_zone_theme_css .='}';
+    }else if($educational_zone_footer_widget_content_alignment == 'Right'){
+        $educational_zone_theme_css .='#colophon ul, #colophon p, .tagcloud, .widget{';
+            $educational_zone_theme_css .='text-align: right;';
+        $educational_zone_theme_css .='}';
+    }
+
+    /*--------------------------- Copyright Content Alignment -------------------*/
+
+    $educational_zone_copyright_content_alignment = get_theme_mod( 'educational_zone_copyright_content_alignment','Right');
+    if($educational_zone_copyright_content_alignment == 'Left'){
+        $educational_zone_theme_css .='.footer-menu-left{';
+        $educational_zone_theme_css .='text-align: left;';
+        $educational_zone_theme_css .='}';
+    }else if($educational_zone_copyright_content_alignment == 'Center'){
+        $educational_zone_theme_css .='.footer-menu-left{';
+            $educational_zone_theme_css .='text-align: center;';
+        $educational_zone_theme_css .='}';
+    }else if($educational_zone_copyright_content_alignment == 'Right'){
+        $educational_zone_theme_css .='.footer-menu-left{';
+            $educational_zone_theme_css .='text-align: right;';
+        $educational_zone_theme_css .='}';
+    }
---
+++ educational-zone/home.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * The blog posts index template file
+ *
+ * If the user has selected  your latest blog posts for their homepage, this is what will
+ * appear.
+ * Learn more: https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Educational Zone
+ */
+
+get_header();
+
+$educational_zone_post_page_title =  get_theme_mod( 'educational_zone_post_page_title', 1 );
+$educational_zone_post_page_meta =  get_theme_mod( 'educational_zone_post_page_meta', 1 );
+$educational_zone_post_page_thumb = get_theme_mod( 'educational_zone_post_page_thumb', 1 );
+$educational_zone_post_page_content =  get_theme_mod( 'educational_zone_post_page_content', 1 );
+?>
+
+    <div id="primary" class="content-area col-sm-12 <?php echo is_active_sidebar('sidebar-1') ? "col-lg-8" : "col-lg-12"; ?>">
+        <main id="main" class="site-main">
+            <div class="card-columns">
+                <?php
+
+                if (have_posts()) :
+
+                while (have_posts()) :the_post(); ?>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <div class="card module-border-wrap hvr-glow">
+                            <?php if ($educational_zone_post_page_thumb == 1 ) {?>
+                                <?php if(has_post_thumbnail()){
+                                the_post_thumbnail();
+                                } else{?>
+                                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/slider.png" alt="" />
+                              <?php } ?>
+                            <?php }?>
+
+                            
+
+                            <div class="card-body">
+                                <?php if ($educational_zone_post_page_title == 1 ) {?>
+                                    <h3 class="entry-title card-title ">
+                                        <a href="<?php the_permalink() ?>">
+                                            <?php the_title() ?>
+                                        </a>
+                                    </h3><!-- .entry-tile -->
+                                    <hr>
+                                <?php }?>
+                                
+                                <?php if ($educational_zone_post_page_meta == 1 ) {?>
+                                    <p class="entry-meta">
+                                        <small><?php educational_zone_posted_on(); ?></small>
+                                    </p><!-- .entry-meta -->
+                                <?php }?>
+                                <?php if ($educational_zone_post_page_content == 1 ) {?>
+                                    <p class="entry-summary">
+                                        <?php
+                                        the_excerpt();
+                                        ?>
+                                    </p><!-- .entry-summary -->
+                                <?php }?>
+                            </div>
+                        </div>
+                    </article>
+                    <!-- #post -->
+                <?php endwhile; // End of the loop.
+                ?>
+            </div>
+
+            <div class="col-lg-12 my-5">
+                <?php the_posts_pagination(); ?>
+
+            </div>
+
+            <?php endif; ?>
+        </main><!-- #main -->
+    </div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
